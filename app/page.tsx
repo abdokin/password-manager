babfa0ef76b26fde0e7aca96e00e6697d4777ec8
abdokin <bashbin.sh@gmail.com>
2024-03-06 20:28:25 +0100

    index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import PasswordsList from "@/components/passswords-list";
+import { Search } from "@/components/search";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="container">
-        <h1>Password Manager!!</h1>
+    <main className="container py-8 max-w-3xl">
+      <div className="flex justify-between">
+        <h1>149 sites and apps</h1>
+        <Search />
+      </div>
+      <PasswordsList />
     </main>
   );
 }
