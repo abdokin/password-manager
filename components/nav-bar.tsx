9bdb979890f60d78254f8a744fcadf0cea0d4097
abdokin <bashbin.sh@gmail.com>
2024-03-06 20:27:28 +0100

    add navbar

---
+++ components/nav-bar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div className="container flex w-full justify-between py-8 border-b">
+      <h1>Password Managaer</h1>
+      <p>profile</p>
+    </div>
+  );
+}
