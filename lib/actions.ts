3c2dc85eb21bf628d2d67b576b3b7423d7a2b1ae
abdokin <bashbin.sh@gmail.com>
2024-03-09 14:50:35 +0100

    update & delete

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,9 +1,15 @@
 "use server";
 import { db } from "@/data";
 import { z } from "zod";
-import { insertPasswordSchema, passwordsTable } from "@/data/schema";
+import {
+  insertPasswordSchema,
+  passwordsTable,
+  updatePasswordSchema,
+} from "@/data/schema";
 import { v4 as uuidv4 } from "uuid";
 import { revalidatePath } from "next/cache";
+import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 export async function createPassword(
   data: z.infer<typeof insertPasswordSchema>
@@ -15,14 +21,49 @@ export async function createPassword(
     const slug = uuidv4();
 
     await db.insert(passwordsTable).values({
-      name:validateData.name,
-      username:validateData.username,
-      password:validateData.password,
+      name: validateData.name,
+      username: validateData.username,
+      password: validateData.password,
       slug,
     });
     revalidatePath("/");
     return {
       message: "Password Created!!",
+    };
+  } catch (error: any) {
+    return {
+      error: error.message,
+    };
+  }
+}
+
+export async function deletePassword(id: number) {
+  await db.delete(passwordsTable).where(eq(passwordsTable.id, id));
+  redirect(`/`);
+}
+
+export async function editPassword(
+  id: number,
+  data: z.infer<typeof updatePasswordSchema>
+) {
+  try {
+    await updatePasswordSchema.parseAsync(data);
+    await db
+      .update(passwordsTable)
+      .set({
+        ...data,
+      })
+      .where(eq(passwordsTable.id, id));
+    const [{ slug }] = await db
+      .select({ slug: passwordsTable.slug })
+      .from(passwordsTable)
+      .where(eq(passwordsTable.id, id))
+      .limit(1);
+    console.log(slug);
+
+    revalidatePath("/password/" + slug, "page");
+    return {
+      message: "Password Updated!!",
     };
   } catch (error: any) {
     return {
