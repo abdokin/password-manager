18c9f700247a4a2f15bb66c70150f8b09704cb8c
abdokin <bashbin.sh@gmail.com>
2024-03-09 14:49:51 +0100

    shadow

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function NavBar() {
   return (
-    <div className="container flex w-full justify-between py-8 border-b">
-      <h1>Password Managaer</h1>
-      <p>profile</p>
+    <div className="container max-w-3xl flex w-full justify-between py-8 border-b shadow-sm">
+      <Link href={"/"}>
+        <h1>Password Managaer</h1>
+      </Link>
     </div>
   );
 }
