cc98f1acad0035a5d4b2c0027e36eca40a575a3e
abdokin <bashbin.sh@gmail.com>
2024-03-09 14:49:24 +0100

    fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,29 @@ import { passwordsTable } from "@/data/schema";
 import { count } from "drizzle-orm";
 
 export default async function Home() {
-  const data = await db
+  const [data] = await db
     .select({ count: count() })
     .from(passwordsTable)
     .limit(1);
   return (
     <main className="container py-8 max-w-3xl">
-      <div className="flex justify-between">
-        <h1>{data[0].count} sites and apps</h1>
-        <div className="flex gap-1 items-center">
-          <Search />
+      {data.count > 0 ? (
+        <>
+          <div className="flex justify-between">
+            <h1>{data.count} sites and apps</h1>
+            <div className="flex gap-1 items-center">
+              <Search />
+              <NewPassword />
+            </div>
+          </div>
+          <PasswordsList />
+        </>
+      ) : (
+        <div className="mx-auto flex gap-2 items-center w-fit">
+          <p>No Password Found</p>
           <NewPassword />
         </div>
-      </div>
-      <PasswordsList />
+      )}
     </main>
   );
 }
