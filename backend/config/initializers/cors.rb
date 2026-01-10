80b25eb07d13d83981d5f6468e48fae381cf4e58
Abdo <abdo@password-manager.local>
2026-01-10 17:53:20 +0100

    Fix CORS initializer and complete payments API integration

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,11 +1,20 @@
-if EnvConfig.cors_enabled?
-  Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins EnvConfig.allowed_origins
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
+Rails.application.config.after_initialize do
+  if defined?(EnvConfig) && EnvConfig.cors_enabled?
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins EnvConfig.allowed_origins
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          credentials: true
+      end
+    end
+  else
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
     end
   end
 end
