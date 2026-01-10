c87f9916b2726049d0b1e1128fdd884a84098a12
Abdo <abdo@password-manager.local>
2026-01-10 17:41:24 +0100

    Add rack-attack middleware to application config

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -8,5 +8,7 @@ module PasswordManager
     config.load_defaults 8.1
     config.api_only = true
     config.action_controller.default_protect_from_forgery = false
+    
+    config.middleware.use Rack::Attack
   end
 end
